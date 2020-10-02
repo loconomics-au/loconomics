4b63a53c493f0c2a5b4d46ebf253bc75c47bcdd2
Sam Waterhouse <samwa_oz@yahoo.co.uk>
2020-10-02 12:37:04 +1000

    #45 add mocked response for help articles

---
+++ web/App_Code/LcRest/HelpArticle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LcRest
+{
+    /// <summary>
+    /// Summary description for HelpArticle
+    /// </summary>
+    public class HelpArticle
+    {
+        #region fields
+        public int author_id;
+        public bool draft;
+        public int id;
+        public string locale;
+        public int permission_group_id;
+        public string title;
+        public string body;
+        public int user_segment_id;
+
+        #endregion
+        public HelpArticle()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        public static IEnumerable<HelpArticle> GetFullList(int languageID, int countryID)
+        {
+            return new List<HelpArticle>() { new HelpArticle { id = 1, user_segment_id = 123, title = "test title", body = "test body" } };
+        }
+    }
+}
