b9332e3c499a6b4834d573106c5b173eaf7a9aa2
Sam Waterhouse <samwa_oz@yahoo.co.uk>
2021-02-08 22:34:26 +1100

    #50 publish settings for localdev

---
+++ build.bat
@@ -0,0 +1,7 @@
+Call yarn --cwd app/ grunt build-webapp
+
+C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild web/website.publishproj^
+	 /p:DeployOnBuild=true /p:PublishProfile="Publish Local" /p:Configuration=localdebug /p:publishUrl=C:\Sites\loconomics
+
+pause
+
