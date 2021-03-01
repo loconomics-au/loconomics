1fc98e23e5d3e435418498d68910f249d93957cc
Sam Waterhouse <samwa_oz@yahoo.co.uk>
2021-03-01 19:30:14 +1100

    #55 update localdev api url

--- app/grunt/shared/buildConfigurations.js
+++ app/grunt/shared/buildConfigurations.js
@@ -4,7 +4,7 @@ module.exports = function(grunt, configuration) {
     
     var configs = { 
         local: { apiUrl: 'http://localhost/loconomics-api', sitePath: "/loconomics/" }, 
-        localdev: { apiUrl: 'https://localdev.loconomics.com.au', sitePath: "/" }, 
+        localdev: { apiUrl: 'https://localapi.loconomics.com.au', sitePath: "/" }, 
         dev: { apiUrl: 'https://app-loconomics-dev-001.azurewebsites.net', sitePath: "/" }, 
         live: { apiUrl: 'https://app-loconomics-prod-001.azurewebsites.net', sitePath: "/" }
     };
