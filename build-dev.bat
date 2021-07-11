ced6633fd71aba1bd4574f9c2e9d91e70538d172
Sam Waterhouse <samwa_oz@yahoo.co.uk>
2021-07-11 16:50:42 +1000

    #is50 add nuget for MSBuild targets

--- build-dev.bat
+++ build-dev.bat
@@ -1,6 +1,6 @@
 Call yarn --cwd app/ grunt build:dev build-webapp
 
 C:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild web/website.publishproj^
-	 /p:DeployOnBuild=true /p:PublishProfile="Publish Local" /p:Configuration=debug /p:publishUrl=C:\Sites\loconomics-dev
+	 /p:DeployOnBuild=true /p:PublishProfile="PublishLocal" /p:Configuration=debug /p:publishUrl=C:\Sites\loconomics-dev
 
 pause
